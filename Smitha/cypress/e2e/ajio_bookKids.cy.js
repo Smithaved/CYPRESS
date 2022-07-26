2675a064150ea700bbbc982581a0991ecb8dddb5
Smithaved <smitha20130@gmail.com>
2022-07-26 10:18:22 +0530

    update

---
+++ Smitha/cypress/e2e/ajio_bookKids.cy.js
@@ -0,0 +1,6 @@
+describe("Ajio",()=>{
+    it("bookKids",()=>{
+        cy.visit("https://www.ajio.com/")
+        cy.title().should("include","Ajio")
+    })
+})
--- Smitha/cypress/e2e/vtiger_CreateOrganizationWithIndustries.cy.js
+++ Smitha/cypress/e2e/vtiger_CreateOrganizationWithIndustries.cy.js
@@ -1,20 +1,24 @@
 describe("Organization",()=>{
-    it("CreateOrganization",()=>{
+    it("Login",()=>{
         cy.visit("http://localhost:8888/")
         cy.title().should('include',"vtiger")
-        var text=cy.title()
-        console.log(text);
         cy.get('.inputs > :nth-child(2) > input').type("admin")
         cy.get(':nth-child(5) > input').type("admin")
         cy.get('#submitButton').click()
         cy.title().should('include',"Home")
+    })
+    it("CreateOrganization",()=>{
+        
         cy.contains('Organization').click()
         cy.get(':nth-child(1) > table > tbody > tr > [style="padding-right:0px;padding-left:10px;"] > a > img').click()
         cy.get(':nth-child(3) > :nth-child(2) > .detailedViewTextBox').type("TestYantra3")
         cy.get(':nth-child(9) > :nth-child(2) > .small').select("Education")
         cy.get(':nth-child(1) > td > div > .save').click()
         cy.wait(2000)
+          
+    })
+    it("Logout",()=>{
         cy.get('[src="themes/softed/images/user.PNG"]').trigger('mouseover')
-        cy.contains('Sign Out').click()   
+        cy.contains('Sign Out').click() 
     })
 })
